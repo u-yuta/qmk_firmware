18a1c14b1f0b995fe08fac7cc53b2b0f4e05485b
u-yuta <u-yuta@users.noreply.github.com>
2024-02-15 13:37:18 +0900

    add nyquist rev4

---
+++ keyboards/keebio/nyquist/keymaps/u-yuta/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// L/R-paren for JP keycodes
+#define LSPO_KEYS KC_LSFT, KC_LSFT, KC_8
+#define RSPC_KEYS KC_RSFT, KC_RSFT, KC_9
+
+// Solve SHIFT ignored in Windows Remote Desktop
+#define USB_POLLING_INTERVAL_MS 10
+
+#define TAPPING_TERM 155
+#define COMBO_TERM 40
