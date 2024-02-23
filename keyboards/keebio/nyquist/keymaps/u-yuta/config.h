7301151a027902023263308f47da8df4b154cb80
u-yuta <u-yuta@users.noreply.github.com>
2024-02-23 20:30:50 +0900

    update keymap (colon and minus to base layer, etc)

--- keyboards/keebio/nyquist/keymaps/u-yuta/config.h
+++ keyboards/keebio/nyquist/keymaps/u-yuta/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Mod-Tap for SHIFT keys
-#define LSPO_KEYS KC_LSFT, KC_LSFT, KC_INT1// Tap -> Underscore
+#define LSPO_KEYS KC_LSFT, KC_LSFT, KC_INT1  // Tap -> Underscore
 
 // Solve SHIFT ignored in Windows Remote Desktop
 #define USB_POLLING_INTERVAL_MS 10
--- keyboards/keebio/nyquist/keymaps/u-yuta/keymap.c
+++ keyboards/keebio/nyquist/keymaps/u-yuta/keymap.c
@@ -4,85 +4,80 @@
 enum combos {
   DF_ESC,
   DS_TAB,
-  CV_DEL,
-  JK_MHEN,
-  LSCLN_ENT,
-  KL_HENK,
-  NM_HOME,
+  CV_COMBO,
+  HJ_COMBO,
+  KL_COMBO,
+  JK_COMBO,
+  LCLN_COMBO,
   MCOMM_PGDN,
-  COMMDOT_PGUP,
-  DOTSLSH_END,
-  ZSLSH_CWTOGG
+  COMMDOT_COMBO,
 };
 
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
 const uint16_t PROGMEM ds_combo[] = {KC_D, KC_S, COMBO_END};
 const uint16_t PROGMEM cv_combo[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM hj_combo[] = {KC_H, KC_J, COMBO_END};
 const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM lscln_combo[] = {KC_L, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM nm_combo[] = {KC_N, KC_M, COMBO_END};
+const uint16_t PROGMEM lcln_combo[] = {KC_L, KC_QUOT, COMBO_END};
 const uint16_t PROGMEM mcomm_combo[] = {KC_M, KC_COMM, COMBO_END};
 const uint16_t PROGMEM commdot_combo[] = {KC_COMM, KC_DOT, COMBO_END};
-const uint16_t PROGMEM dotslsh_combo[] = {KC_DOT, KC_SLSH, COMBO_END};
-const uint16_t PROGMEM zslsh_combo[] = {KC_Z, KC_SLSH, COMBO_END};
 
 combo_t key_combos[] = {
   [DF_ESC] = COMBO(df_combo, KC_ESC),
   [DS_TAB] = COMBO(ds_combo, KC_TAB),
-  [CV_DEL] = COMBO(cv_combo, KC_DEL),
-  [JK_MHEN] = COMBO(jk_combo, JP_MHEN),
-  [LSCLN_ENT] = COMBO(lscln_combo, KC_ENT),
-  [KL_HENK] = COMBO(kl_combo, JP_HENK),
-  [NM_HOME] = COMBO(nm_combo, KC_HOME),
+  [CV_COMBO] = COMBO(cv_combo, CW_TOGG),
+  [HJ_COMBO] = COMBO(hj_combo, KC_HOME),
+  [JK_COMBO] = COMBO(jk_combo, KC_BSPC),
+  [KL_COMBO] = COMBO(kl_combo, KC_DEL),
+  [LCLN_COMBO] = COMBO(lcln_combo, KC_END),
   [MCOMM_PGDN] = COMBO(mcomm_combo, KC_PGDN),
-  [COMMDOT_PGUP] = COMBO(commdot_combo, KC_PGUP),
-  [DOTSLSH_END] = COMBO(dotslsh_combo, KC_END),
-  [ZSLSH_CWTOGG] = COMBO(zslsh_combo, CW_TOGG),
+  [COMMDOT_COMBO] = COMBO(commdot_combo, KC_PGUP),
 };
 
 enum layer_names {
     _DEFAULT,
     // _NAV,
     _SYM,  // symbol, cursor
-    _NUM,  // number, cursor
-    _FUN   // function, mouse
+    _SYM2,  // symbol2, cursor
+    _NUM,  // number, mouse
 };
 
+// S(KC_INT3) : JP_PIPE
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEFAULT] = LAYOUT_ortho_5x12(
-        KC_1,         KC_2,    KC_3,    KC_4,    KC_5,             KC_ESC,    KC_DEL,      KC_6,       KC_7,     KC_8,    KC_9,   KC_0,
-        KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,             KC_TAB,    KC_BSPC,     KC_Y,       KC_U,     KC_I,    KC_O,   KC_P,
-        KC_A,         KC_S,    KC_D,    KC_F,    KC_G,             KC_ESC,    KC_ENT,      KC_H,       KC_J,     KC_K,    KC_L,   KC_SCLN,
-        SFT_T(KC_Z),  KC_X,    KC_C,    KC_V,    KC_B,             SC_LSPO,   KC_LSFT,     KC_N,       KC_M,     KC_COMM, KC_DOT, RSFT_T(KC_SLSH),
-        KC_NO,        KC_LGUI, KC_LALT, KC_LCTL, LT(_SYM, KC_SPC), KC_BSPC,   KC_ENT,      MO(_NUM),   MO(_FUN), KC_NO,   KC_NO,  KC_RALT
+        KC_1,         KC_2,    KC_3,    KC_4,           KC_5,             KC_NO,       KC_NO,       KC_6,      KC_7,        KC_8,    KC_9,   KC_0,
+        KC_Q,         KC_W,    KC_E,    KC_R,           KC_T,             KC_TAB,      KC_BSPC,     KC_Y,      KC_U,        KC_I,    KC_O,   KC_P,
+        KC_A,         KC_S,    KC_D,    KC_F,           KC_G,             KC_ESC,      KC_ENT,      KC_H,      KC_J,        KC_K,    KC_L,   JP_COLN,
+        SFT_T(KC_Z),  KC_X,    KC_C,    KC_V,           KC_B,             SC_LSPO,     KC_LSFT,     KC_N,      KC_M,        KC_COMM, KC_DOT, RSFT_T(KC_MINS),
+        KC_NO,        KC_LGUI, KC_LALT, CTL_T(JP_MHEN), LT(_SYM, KC_SPC), KC_BSPC,       KC_ENT,      MO(_SYM2), JP_HENK,     KC_NO,   KC_RALT,KC_NO
     ),
 	[_SYM] = LAYOUT_ortho_5x12(
-        KC_NO,          KC_NO,       KC_NO,       KC_NO,       KC_NO,      KC_NO,     KC_NO,     KC_F6,     KC_NO,     KC_NO,     KC_NO,     KC_NO,
-        JP_EXLM,        JP_CIRC,     JP_HASH,     JP_DLR,      JP_PERC,    KC_NO,     KC_NO,     JP_AMPR,   JP_UNDS,   JP_LPRN,   JP_RPRN,   JP_YEN,
-        JP_QUOT,        JP_DQUO,     JP_LBRC,     JP_RBRC,     JP_ASTR,    KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   JP_COLN,
-        SFT_T(JP_PIPE), JP_AT,       JP_LCBR,     JP_RCBR,     JP_TILD,    KC_NO,     KC_NO,     JP_EQL,    JP_MINS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
-        KC_TRNS,        KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
+        KC_F1,       KC_F2,       KC_F3,      KC_F4,      KC_F5,      KC_F11,    KC_F12,    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,
+        JP_EXLM,     JP_CIRC,     JP_HASH,    JP_DLR,     JP_PERC,    KC_NO,     KC_NO,     JP_AMPR,   JP_UNDS,   JP_LPRN,   JP_RPRN,   JP_YEN,
+        JP_QUOT,     JP_DQUO,     JP_LBRC,    JP_RBRC,    JP_GRV,     KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_SCLN,
+        KC_LSFT,     JP_PIPE,     JP_LCBR,    JP_RCBR,    JP_TILD,    KC_NO,     KC_NO,     JP_QUES,   JP_PLUS,   KC_TRNS,   KC_TRNS,   KC_SLSH,
+        KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
 	),
-	[_NUM] = LAYOUT_ortho_5x12(
-        KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,      KC_TRNS,   KC_F11,    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,
-        JP_EXLM,     KC_7,        KC_8,        KC_9,        JP_PERC,    KC_NO,     KC_NO,     JP_AMPR,   JP_UNDS,   JP_LPRN,   JP_RPRN,   JP_YEN,
-        JP_QUOT,     KC_4,        KC_5,        KC_6,        JP_ASTR,    KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   JP_COLN,
-        SFT_T(KC_0), KC_1,        KC_2,        KC_3,        JP_GRV,     KC_NO,     KC_NO,     JP_EQL,    JP_MINS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+	[_SYM2] = LAYOUT_ortho_5x12(
+        KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,      KC_F11,    KC_F12,    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,
+        JP_EXLM,     JP_CIRC,     JP_HASH,     JP_DLR,      JP_PERC,    KC_NO,     KC_NO,     JP_AMPR,   JP_UNDS,   JP_LPRN,   JP_RPRN,   JP_YEN,
+        JP_QUOT,     JP_DQUO,     JP_LBRC,     JP_RBRC,     JP_GRV,     KC_NO,     KC_NO,     KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_SCLN,
+        CW_TOGG,     JP_AT,       JP_LCBR,     JP_RCBR,     JP_TILD,     KC_NO,     KC_NO,    JP_QUES,   JP_PLUS,   KC_TRNS,   KC_TRNS,   KC_SLSH,
         KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
 	),
-	[_FUN] = LAYOUT_ortho_5x12(
-        KC_NO,      KC_F10,     KC_F11,     KC_F12,     KC_NO,       KC_NO,     KC_F11,    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,
-        KC_NO,      KC_F7,      KC_F8,      KC_F9,      KC_F10,      KC_NO,     KC_NO,     KC_WH_L,   KC_WH_D,   KC_WH_U,   KC_WH_R,   KC_NO,
-        KC_NO,      KC_F4,      KC_F5,      KC_F6,      KC_F11,      KC_NO,     KC_NO,     KC_MS_L,   KC_MS_D,   KC_MS_U,   KC_MS_R,   KC_NO,
-        KC_0,       KC_F1,      KC_F2,      KC_F3,      KC_NO,       KC_NO,     KC_NO,     KC_NO,     KC_BTN1,  KC_BTN3,  KC_BTN2,  KC_TRNS,
+	[_NUM] = LAYOUT_ortho_5x12(
+        KC_F1,      KC_F2,      KC_F3,      KC_F3,      KC_F5,       KC_F11,    KC_F12,    KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,
+        KC_NO,      KC_7,       KC_8,       KC_9,       KC_NO,       KC_NO,     KC_NO,     KC_WH_U,   KC_BTN1,   KC_BTN3,   KC_BTN2,   KC_WH_D,
+        KC_NO,      KC_4,       KC_5,       KC_6,       KC_NO,       KC_NO,     KC_NO,     KC_MS_L,   KC_MS_D,   KC_MS_U,   KC_MS_R,   KC_NO,
+        KC_0,       KC_1,       KC_2,       KC_3,       KC_NO,       KC_NO,     KC_NO,     KC_HOME,   KC_PGDN,   KC_PGUP,   KC_END,    KC_NO,
         KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS
 	)
 };
 
 // SYM + NUM -> FUN
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _SYM, _NUM, _FUN);
+    return update_tri_layer_state(state, _SYM, _SYM2, _NUM);
 }
 
 // not seem to work
