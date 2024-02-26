9d1db492ce3b03d2c3238be65e8e8c3ac450658e
u-yuta <u-yuta@users.noreply.github.com>
2024-02-26 22:15:58 +0900

    treat holding Alt ang GUI on home row as tap

--- keyboards/keebio/nyquist/keymaps/u-yuta/keymap.c
+++ keyboards/keebio/nyquist/keymaps/u-yuta/keymap.c
@@ -45,16 +45,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_DEFAULT] = LAYOUT_ortho_5x12(
         KC_NO,      KC_1,        KC_2,        KC_3,        KC_4,        KC_5,            KC_6,    KC_7,        KC_8,        KC_9,        KC_0,           KC_DEL,
         KC_TAB,     KC_Q,        KC_W,        KC_E,        KC_R,        KC_T,            KC_Y,    KC_U,        KC_I,        KC_O,        KC_P,           KC_MINS,
-        KC_ESC,     LGUI_T(KC_A),LALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,            KC_H,    RSFT_T(KC_J),RCTL_T(KC_K),RALT_T(KC_L),RGUI_T(KC_SCLN),KC_ENT,
+        KC_ESC,     RGUI_T(KC_A),RALT_T(KC_S),LCTL_T(KC_D),LSFT_T(KC_F),KC_G,            KC_H,    RSFT_T(KC_J),RCTL_T(KC_K),RALT_T(KC_L),RGUI_T(KC_SCLN),KC_ENT,
         KC_LSFT,    KC_Z,        KC_X,        KC_C,        KC_V,        KC_B,            KC_N,    KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        KC_RSFT,
         KC_BSPC,    KC_NO,       KC_LGUI,     KC_LALT,     JP_MHEN,     LT(_NAV, KC_SPC),MO(_SYM),JP_HENK,     KC_RCTL,     KC_RALT,     KC_NO,          KC_ENT
     ),
 	[_SYM] = LAYOUT_ortho_5x12(
-        KC_F11,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,    KC_F12,
-        KC_TRNS,    JP_EXLM,    JP_DQUO,    JP_LBRC,    JP_RBRC,    JP_PERC,    KC_NO,      JP_AMPR,    JP_ASTR,    JP_EQL,     JP_YEN,    KC_TRNS,
-        KC_TRNS,    JP_UNDS,    JP_QUOT,    JP_LPRN,    JP_RPRN,    JP_AT,      KC_LEFT,    KC_RSFT,    KC_RCTL,    KC_RGHT,    JP_COLN,   KC_TRNS,
-        KC_TRNS,    JP_CIRC,    JP_DLR,     JP_LCBR,    JP_RCBR,    JP_HASH,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      JP_MINS,   KC_TRNS,
-        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS
+        KC_F11,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,        KC_F8,        KC_F9,        KC_F10,         KC_F12,
+        KC_TRNS,    JP_EXLM,    JP_DQUO,    JP_LBRC,    JP_RBRC,    JP_PERC,    KC_NO,      JP_AMPR,      JP_ASTR,      JP_EQL,       JP_YEN,         KC_TRNS,
+        KC_TRNS,    JP_UNDS,    JP_QUOT,    JP_LPRN,    JP_RPRN,    JP_AT,      KC_LEFT,    OSM(MOD_RSFT),OSM(MOD_RCTL),OSM(MOD_RALT),RGUI_T(JP_COLN),KC_TRNS,
+        KC_TRNS,    JP_CIRC,    JP_DLR,     JP_LCBR,    JP_RCBR,    JP_HASH,    KC_NO,      KC_NO,        KC_NO,        KC_NO,        JP_MINS,        KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,        KC_TRNS
 	),
 	[_NAV] = LAYOUT_ortho_5x12(
         KC_F11,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,    KC_F12,
@@ -70,6 +70,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _SYM, _NAV, _NUM);
 }
 
+// settings for CAPS_WORD
 bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
         // Keycodes that continue Caps Word, with shift applied.
@@ -90,11 +91,23 @@ bool caps_word_press_user(uint16_t keycode) {
     }
 }
 
+// This treats the tapping term as infinite, so it won't resort to alt unless hit another key.
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RALT_T(KC_L):
+    case RGUI_T(KC_SCLN):
+      return -1;
+    default:
+      return TAPPING_TERM;
+  }
+}
+
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     if (!process_achordion(keycode, record)) { return false; }
