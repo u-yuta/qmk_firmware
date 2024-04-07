a27e76b52254494e3307178460ad8af5e3acd3b5
u-yuta <u-yuta@users.noreply.github.com>
2024-04-07 23:20:11 +0900

    port nyquist keymap to piantor (WIP)

--- keyboards/beekeeb/piantor/keymaps/u-yuta/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/u-yuta/keymap.c
@@ -2,15 +2,79 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "keymap_extras/keymap_japanese.h"
+#include "features/achordion.h"
 
-enum combos {
-    DF_COMBO,
+enum layer_names {
+    _DEFAULT,
+    _NAV,
+    _NUM,
+    _SYM,
+    _SYM2,
+    _MOUSE
 };
 
+// Alias -- home row
+#define HOME_A LT(_SYM, KC_A)
+#define HOME_SCLN LT(_SYM2, KC_SCLN)
+// Alias -- bottom row
+#define BTM_C CTL_T(KC_C)
+#define BTM_V SFT_T(KC_V)
+#define BTM_M SFT_T(KC_M)
+#define BTM_COMM CTL_T(KC_COMM)
+
+// Alias -- Mod-Tap
+#define ENT_NUM LT(_NUM, KC_ENT)
+#define MHEN_CTL CTL_T(KC_INT5)  // JP_MHEN
+
+enum combos {
+    RT_COMBO,
+    SD_COMBO,
+    DF_COMBO,
+    ZX_COMBO,
+    XC_COMBO,
+    CV_COMBO,
+    ZC_COMBO,
+    ZV_COMBO,
+    FG_COMBO,
+    HJ_COMBO,
+    JK_COMBO,
+    KL_COMBO,
+    LSCLN_COMBO,
+    COMMDOT_COMBO,
+    DOTSLSH_COMBO,
+    NUM_COMBO,
+};
+
+const uint16_t PROGMEM rt_combo[] = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM zx_combo[] = {KC_Z, KC_X, COMBO_END};
+const uint16_t PROGMEM xc_combo[] = {KC_X, BTM_C, COMBO_END};
+const uint16_t PROGMEM cv_combo[] = {BTM_C, BTM_V, COMBO_END};
+const uint16_t PROGMEM fg_combo[] = {KC_F, KC_G, COMBO_END};
+const uint16_t PROGMEM hj_combo[] = {KC_H, KC_J, COMBO_END};
+const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM lscln_combo[] = {KC_L, HOME_SCLN, COMBO_END};
+const uint16_t PROGMEM commdot_combo[] = {BTM_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM dotslsh_combo[] = {KC_DOT, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM num_combo[] = {KC_N, KC_U, KC_M, COMBO_END};
 
 combo_t key_combos[] = {
+    [RT_COMBO] = COMBO(rt_combo, CW_TOGG),
+    [SD_COMBO] = COMBO(sd_combo, KC_TAB),
     [DF_COMBO] = COMBO(df_combo, KC_ESC),
+    [ZX_COMBO] = COMBO(zx_combo, KC_LGUI),
+    [XC_COMBO] = COMBO(xc_combo, KC_LALT),
+    [FG_COMBO] = COMBO(fg_combo, JP_MHEN),
+    [HJ_COMBO] = COMBO(hj_combo, JP_HENK),
+    [JK_COMBO] = COMBO(jk_combo, KC_BSPC),
+    [KL_COMBO] = COMBO(kl_combo, KC_DEL),
+    [LSCLN_COMBO] = COMBO(lscln_combo, JP_COLN),
+    [COMMDOT_COMBO] = COMBO(commdot_combo, KC_RALT),
+    [DOTSLSH_COMBO] = COMBO(dotslsh_combo, KC_RGUI),
+    [NUM_COMBO] = COMBO(num_combo, KC_NUM),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,10 +92,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                   └───┤   │   │   ├───┘
       *                       └───┘   └───┘
       */
-    [0] = LAYOUT_split_3x6_3(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LGUI, KC_BSPC, KC_SPC,           KC_SPC,  KC_ENT,  KC_RALT
-    )
+    [_DEFAULT] = LAYOUT_split_3x6_3(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_MINS,
+        KC_ESC,  HOME_A,  KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    HOME_SCLN,  KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    BTM_C,   BTM_V,   KC_B,                      KC_N,    BTM_M,   BTM_COMM,KC_DOT,  KC_SLSH,    KC_RSFT,
+                                            KC_LALT, KC_LCTL,KC_SPC,  ENT_NUM,  MO(_NAV),KC_LEFT
+    ),
+    // navigation
+	[_NAV] = LAYOUT_split_3x6_3(
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
+                                            _______, _______,_______, _______,  _______, _______
+	)
 };
+
+// Restrict oppisite hand holds by mod-tap
+bool achordion_chord(uint16_t tap_hold_keycode,
+                    keyrecord_t* tap_hold_record,
+                    uint16_t other_keycode,
+                    keyrecord_t* other_record) {
+
+    switch (tap_hold_keycode) {
+        // case BTM_SLSH:
+        //     if (other_keycode == KC_H || other_keycode == KC_J || other_keycode == KC_K || other_keycode == KC_L)
+        //         { return true; }
+        //         break;
+        case MO(_NAV):  // Allow all
+        case ENT_NUM:  // Allow all
+            return true;
+    }
+    switch (other_keycode) {
+        case KC_SPC:
+        case KC_ENT:
+            return true;
+    }
+    // Allow same-hand holds when the other key is in the rows below the
+    // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
+    // if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) { return true; }
+
+    // Otherwise, follow the opposite hands rule.
+    return achordion_opposite_hands(tap_hold_record, other_record);
+}
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    if (!process_achordion(keycode, record)) { return false; }
+
+    return true;
+}
+
+void matrix_scan_user(void) {
+    achordion_task();
+}
