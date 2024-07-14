30ca5f10e2ddaf714f3a893bad06ee77a2f3c073
u-yuta <u-yuta@users.noreply.github.com>
2024-07-14 18:59:56 +0900

    feat: decrease TAPPING_TERM

--- keyboards/beekeeb/piantor/keymaps/u-yuta/config.h
+++ keyboards/beekeeb/piantor/keymaps/u-yuta/config.h
@@ -6,7 +6,7 @@
 // Solve SHIFT ignored in Windows Remote Desktop
 #define USB_POLLING_INTERVAL_MS 10
 
-#define TAPPING_TERM 200
+#define TAPPING_TERM 170
 #define TAPPING_TERM_PER_KEY
 #define COMBO_TERM 32
 #define PERMISSIVE_HOLD_PER_KEY
