139cfc793c16ce264d67005cd9913b8fff54657a
u-yuta <u-yuta@users.noreply.github.com>
2024-07-14 19:09:53 +0900

    feat: change ENTER at mid 6th column to COLON

--- keyboards/beekeeb/piantor/keymaps/u-yuta/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/u-yuta/keymap.c
@@ -104,28 +104,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
     [_DEFAULT] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_MINS,
-        KC_ESC,  HOME_A,  KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    HOME_SCLN,KC_ENT,
+        KC_ESC,  HOME_A,  KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    HOME_SCLN,JP_COLN,
         KC_LSFT, KC_Z,    KC_X,    BTM_C,   BTM_V,   KC_B,                      KC_N,    BTM_M,   BTM_COMM,KC_DOT,  KC_SLSH,  JP_BSLS,
                                             KC_LALT, KC_LCTL, KC_SPC,  ENT_NUM, MO(_NAV),KC_LEFT
     ),
     // navigation
     [_NAV] = LAYOUT_split_3x6_3(
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
                                             _______, _______, _______, _______, _______, _______
     ),
     // num
     [_NUM] = LAYOUT_split_3x6_3(
         KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F4,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
         XXXXXXX, KC_Z,    KC_X,    BTM_C,   BTM_V,   XXXXXXX,                   XXXXXXX, BTM_M,   BTM_COMM,KC_DOT,  KC_SLSH, JP_BSLS,
                                             _______, _______, _______, _______, _______, _______
     ),
     // symbol 1
     [_SYM] = LAYOUT_split_3x6_3(
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   JP_CIRC, JP_AMPR, JP_ASTR, JP_PIPE, XXXXXXX, XXXXXXX,
-        _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   JP_QUOT, JP_LPRN, JP_RPRN, JP_TILD, JP_COLN, _______,
+        _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   JP_QUOT, JP_LPRN, JP_RPRN, JP_TILD, JP_PLUS, JP_ASTR,
         _______, OSM_RGUI,OSM_LALT,OSM_LCTL,OSM_LSFT,XXXXXXX,                   XXXXXXX, JP_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                             _______, _______, _______, _______, _______, _______
     ),
